bc480fb60558d47097f7eaba3f33181e6e7524ed
ramonmonmorenocesur <ramon.s384505@cesurformacion.com>
2023-11-08 10:04:36 +0100

    Subido abecedario&rombo

---
+++ src/abecedario.java
@@ -0,0 +1,14 @@
+
+public class abecedario {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	 
+		for (int fila = 97; fila < 123; fila++){
+			System.out.print((char) fila);
+		}
+		
+	}
+
+}
