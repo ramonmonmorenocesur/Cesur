7ba0d24a078e9b16fb213444fc3159d01f0a60ca
ramonmonmorenocesur <ramon.s384505@cesurformacion.com>
2023-11-14 12:47:59 +0100

    seguirencasa

--- src/contadorinverso.java
+++ src/contadorinverso.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class contadorinverso {
 
@@ -12,11 +13,35 @@ public class contadorinverso {
 		for (int numero = 1200; numero >= 620; numero = numero - 20) {
 			System.out.print(numero + " ");
 		}
-		System.out.println(); numeroInicial=1200;
+		System.out.println();
+		numeroInicial = 1200;
 		do {
-			System.out.print(numeroInicial+ " ");
-		numeroInicial=numeroInicial-20;
-		} while(numeroInicial>=620);
+			System.out.print(numeroInicial + " ");
+			numeroInicial = numeroInicial - 20;
+		} while (numeroInicial >= 620);
+		System.out.println();
+		// vamos a pedir ahora nuevas cosas
+
+		Scanner teclado = new Scanner(System.in);
 		
+		System.out.print("Introduzca el max: ");
+		int variableMax = teclado.nextInt();
+		
+		int variableMin;
+		do {
+			System.out.print("Introduzca el mim: ");
+			variableMin = teclado.nextInt();
+		} while (variableMax<variableMin);
+		
+		
+		
+		System.out.print("Introduzca el Salto: ");
+		int salto = teclado.nextInt();
+		
+		do {
+			variableMax=variableMax - salto;
+			System.out.println(variableMax+" ");
+			}while(variableMax>variableMin);
+	teclado.close();
 	}
-	}
+}
